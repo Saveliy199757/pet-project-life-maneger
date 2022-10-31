50daf8dd3a611336efcf2a4d0e244937ede0b8a0
saveliyB <saveliy199757@gmail.com>
2022-10-31 15:14:42 +0300

    add all routes, open/close sidenav + animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface sidenavToggle {
+  screenWidth: number,
+  collapsed: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ui-responsive-sidenav';
+  isSidenavCollapsed = false
+  screenWidth = 0
+  onToggleSidenav(data: sidenavToggle): void {
+    this.isSidenavCollapsed = data.collapsed
+    this.screenWidth = data.screenWidth
+  }
 }
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,76 @@
+import {Component, EventEmitter, HostListener, OnInit, Output} from '@angular/core';
+import {navbarData} from "./nav-data";
+import {animate, keyframes, style, transition, trigger} from "@angular/animations";
+
+interface sidenavToggle {
+  screenWidth: number,
+  collapsed: boolean
+}
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate('350ms',
+          style({opacity: 1})
+          )
+      ]),
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('350ms',
+          style({opacity: 0})
+        )
+      ])
+    ]),
+    trigger('rotate', [
+      transition(':enter', [
+        animate('1000ms',
+          keyframes([
+            style({transform: 'rotate(0deg)', offset: '0'}),
+            style({transform: 'rotate(2turn)', offset: '1'})
+            ]
+          )
+        )
+      ])
+    ])
+  ]
+})
+export class SidenavComponent implements OnInit {
+
+  @Output() onToggleSidenav: EventEmitter<sidenavToggle> = new EventEmitter()
+
+  constructor() { }
+
+  collapsed = true
+  navData = navbarData
+  screenWidth = 0
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = window.innerWidth
+    if (this.screenWidth <= 768) {
+      this.collapsed = false
+      this.onToggleSidenav.emit({screenWidth: this.screenWidth, collapsed: this.collapsed})
+    }
+  }
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth
+  }
+
+  toggleSidenav(): void {
+    this.collapsed = !this.collapsed
+    this.onToggleSidenav.emit({screenWidth: this.screenWidth, collapsed: this.collapsed})
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false
+    this.onToggleSidenav.emit({screenWidth: this.screenWidth, collapsed: this.collapsed})
+  }
+
+
+}
